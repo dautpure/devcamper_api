9a5ddaa503d8fe526855df90619d7d329d57ddc7
Shekhar <sdautpure@appirio.com>
2020-12-30 02:52:20 +0100

    Added db config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
-    const conn = await mongoose.connect();
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true
+    });
+
+    console.log(`Mongoose db connected: ${conn.connection.host}`);
 };
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-//Bringing Route Files
-const bootcamps = require('./routes/bootcamps');
-
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({ path: './config/config.env' });
+
+//Connect to DB
+connectDB();
+
+//Bringing Route Files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -26,6 +29,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error : ${err.message}`);
+    server.close(() => process.exit(1));
 });
