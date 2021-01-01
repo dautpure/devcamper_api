0739c8ccf325d76a7c3e050dcb920563a84e5cd0
Shekhar <sdautpure@appirio.com>
2021-01-01 22:29:30 +0100

    Added Casting error

--- middleware/error.js
+++ middleware/error.js
@@ -1,15 +1,21 @@
+const ErrorResponse = require('../utils/errorResponse');
+
 const errorHandler = (err, req, res, next) => {
-    console.log(err.stack);
+
+    let error = {...err };
+    error.message = err.message;
+
+    // console.log(err.stack);
 
     //Mongoose object not found 
     if (err.name === 'CastError') {
         const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
     }
 
-    console.log(err.name);
-    res.status(err.statusCode || 500).json({
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     });
 };
 module.exports = errorHandler;
